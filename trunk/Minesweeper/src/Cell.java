06563ef9f1e4c93115876de7dc553dc6ee7175bb
vincentgarcia00@gmail.com <vincentgarcia00@gmail.com@cf664220-5a67-da83-aeba-500869ba4912>
2012-03-16 18:12:24 +0000

    Committed new Minesweeper folder with changes to the way cells are drawn

---
+++ trunk/Minesweeper/src/Cell.java
@@ -0,0 +1,17 @@
+
+public class Cell {
+	
+	private int value;
+	private int imageIndex;
+	private boolean isUncovered;
+	private boolean isFlagged;
+	
+	public Cell() { value = 0; imageIndex = 9; isUncovered = isFlagged = false;}
+	public Cell(int val) { value = val; imageIndex = 9; isUncovered = isFlagged = false; }
+	public int getValue() { return value; }
+	public int getImageIndex() { return imageIndex; }
+	public boolean getIsUncovered() { return isUncovered; }
+	public boolean getIsFlagged() { return isFlagged; }
+	public void setValue(int c) { value = c; }
+	public void setImageIndex(int c) { imageIndex = c; }
+}
