cb47fb04ab4b0f893946cb839821e3b92e436c8d
allenma99@gmail.com <allenma99@gmail.com@cf664220-5a67-da83-aeba-500869ba4912>
2012-04-10 14:06:42 +0000

    Added report file

--- JUnit/TestRunner.java
+++ JUnit/TestRunner.java
@@ -8,17 +8,61 @@ import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
 public class TestRunner {
+	File file;
 
 	@Test
 	public void RunAll() {
 		
-		Result result = JUnitCore.runClasses(CellTest.class);
-		for (Failure failure : result.getFailures()) {
-			System.out.println(failure.toString());
+		Formatter inputfile;
+		int counter = 0;
+		file = new File("ErrorReport.txt");
+		
+		if(!file.exists()){	
+			try {
+				inputfile = new Formatter("ErrorReport.txt");
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+			
+			return;
 		}
-		result = JUnitCore.runClasses(BeginnerGridTest.class);
-		for (Failure failure : result.getFailures()) {
-			System.out.println(failure.toString());
+		
+		Result result = JUnitCore.runClasses(CellTest.class); //Test cell class
+		
+
+			try {
+				inputfile = new Formatter("ErrorReport.txt");
+				for (Failure failure : result.getFailures()) {
+					//System.out.println(failure.toString());
+					inputfile.format("%s", failure.toString());
+					counter += result.getRunCount();
+				}
+
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+
+		
+		result = JUnitCore.runClasses(BeginnerGridTest.class); //Test beginner grid
+
+			try {
+				inputfile = new Formatter("ErrorReport.txt");
+				for (Failure failure : result.getFailures()) {
+					inputfile.format("%s", failure.toString());
+					counter += result.getRunCount();
+				}
+
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
 		}
+		
+			try {
+				inputfile = new Formatter("ErrorReport.txt");
+				inputfile.format("%x tests completed", counter);
+				inputfile.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+			
 	}
 }
