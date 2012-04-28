b53465a1b2db0ba6ef1fa9c0a56ae4c6a814e968
prachirp <prachirp@cf664220-5a67-da83-aeba-500869ba4912>
2012-04-28 18:31:40 +0000

    Working Triangle Code without MinesAdapter

--- trunk/Shane's code/TriangleCell.java
+++ trunk/Shane's code/TriangleCell.java
@@ -1,62 +1,89 @@
 public class TriangleCell
 {
+	
+	private int X;
+	private int Y;
 	private int value;
 	private int imageIndex;
 	private boolean isUncovered;
 	private boolean isFlagged;
+	private boolean isUp;
+	private boolean isBomb;
+	private ArrayList<TriangleCell> Adjacent;
 	
-	public TriangleCell(int imageIndex)
-	{ value=0; this.imageIndex=imageIndex; isUncovered=isFlagged=false; }
 	
-	public int getValue()
-	{ return value; }
+	public TriangleCell() { X=Y=-1;value = 0; imageIndex = 0; isUncovered = isFlagged = isBomb = false; isUp = null;Adjacent=new ArrayList<TriangleCell>();}
+	public TriangleCell(int x, int y) {
+		imageIndex = 9; 
+		isUncovered = isFlagged = isBomb = false;
+		if((x+y)%2==0){
+			isUp = true;
+			value = 0;
+		}
+		else{
+			isUp = false;
+			value = 1;
+		}		
+		X=x;
+		Y=y;
+		Adjacent=new ArrayList<TriangleCell>();
+	}
 	
-	public void setValue(int value)
-	{ this.value=value; }
+	public int getValue() { return value; }
+	public int getImageIndex() { return imageIndex; }
+	public int getX() { return X;}
+	public int getY() { return Y;}
+	public ArrayList<TriangleCell> getAdjacentArray() { return Adjacent;}
+	public boolean isUncovered() { return isUncovered; }
+	public boolean isFlagged() { return isFlagged; }
+	public boolean isBomb(){return isBomb;}
+	public void setValue(int c) { value = c; if(c == 28 || c == 29){isBomb = true;}}
+	public void setImageIndex(int c) { imageIndex = c; }
+	public void setUncovered(boolean b) { isUncovered = b; }
+	public void setFlagged(boolean b) { isFlagged = b; }
+	public void setX(int x){ X=x;}
+	public void setY(int y){ Y=y;}
 	
-	public int getImageIndex()
-	{ return imageIndex; }
-	
-	public void setImageIndex(int imageIndex)
-	{ this.imageIndex=imageIndex; }
-	
-	public boolean getIsUncovered()
-	{ return isUncovered; }
-	
-	public void setIsUncovered(boolean b)
-	{ isUncovered=b; }
-	
-	public boolean getIsFlagged()
-	{ return isFlagged; }
-	
-	public void setIsFlagged(boolean b)
-	{ isFlagged=b; }
 }
 
 //for imageIndex, difficult to understand because there's two images for everything
 //because there's an entire different set of images for the upside down triangles
 //img=val
-//0 = 1 UP triangle
-//1 = 1 DOWN triangle
-//2 = 2 UP triangle
-//3 = 2 DOWN triangle
-//4 = 3 UP triangle
-//5 = 3 DOWN triangle
-//6 = 4 UP triangle
-//7 = 4 DOWN triangle
-//8 = 5 UP triangle
-//9 = 5 DOWN triangle
-//10 =6 UP triangle
-//11 =6 DOWN triangle
-//12 = empty UP
-//13 = empty DOWN
-//14 = covered UP
-//15 = covered DOWN
-//16 = bomb UP
-//17 = bomb DOWN
-//18 = red bomb UP
-//19 = red bomb DOWN
-//20 = flag UP
-//21 = flag DOWN
-//22 = wrong flag UP
-//23 = wrong flag DOWN
+//0 = empty UP
+//1 = empty DOWN
+//2 = 1 UP triangle
+//3 = 1 DOWN triangle
+//4 = 2 UP triangle
+//5 = 2 DOWN triangle
+//6 = 3 UP triangle
+//7 = 3 DOWN triangle
+//8 = 4 UP triangle
+//9 = 4 DOWN triangle
+//10 = 5 UP triangle
+//11 = 5 DOWN triangle
+//12 = 6 UP triangle
+//13 = 6 DOWN triangle
+//14 = 7 UP triangle
+//15 = 7 DOWN triangle
+//16 = 8 UP triangle
+//17 = 8 DOWN triangle
+//18 = 9 UP triangle
+//19 = 9 DOWN triangle
+//20 = 10 UP triangle
+//21 = 10 DOWN triangle
+//22 = 11 UP triangle
+//23 = 11 DOWN triangle
+//24 = 12 UP triangle
+//25 = 12 DOWN triangle
+//26 = covered UP
+//27 = covered DOWN
+//28 = bomb UP
+//29 = bomb DOWN
+//30 = red bomb UP
+//31 = red bomb DOWN
+//32 = flag UP
+//33 = flag DOWN
+//34 = wrong flag UP
+//35 = wrong flag DOWN
+
+
